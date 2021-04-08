5c6d7242c9bebdcd3c873bf57b62db490a886b21
Forest Guhareza <arza.3d@gmail.com>
2021-04-09 03:26:02 +0700

    2 + 2

--- index.php
+++ index.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>TEST</h1>
-    </body>
-</html>
+<?php
+    echo 2 + 2;
+?>
